a9c354993fbd051bb008973340cf04ee2b5a5ebd
Laurindo <netoj3849@gmail.com>
2022-12-04 16:31:14 -0300

    Envio final

---
+++ routes/funcionarioRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const routes = express.Router();
+
+const funcionarioController = require("../controllers/funcionarioController");
+const auth = require("../middlewares/fornecedorAuth");
+
+
+routes.get("/funcionario", auth, funcionarioController.listar);
+routes.get("/funcionario/relatorio", auth, funcionarioController.relatorio);
+routes.post("/funcionario", funcionarioController.cadastrarPost);
+routes.get("/funcionario/cadastrar/:codigo?", auth, funcionarioController.cadastrarGet);
+routes.get("/funcionario/:codigo", auth, funcionarioController.detalhar);
+routes.get("/funcionario/remover/:codigo", auth, funcionarioController.remover);
+
+module.exports = routes;
